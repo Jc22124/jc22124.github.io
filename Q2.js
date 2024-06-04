bb36ab188d73feb00087aeaf6ceca78f66bd7eea
Jc22124 <154946206+Jc22124@users.noreply.github.com>
2024-06-04 19:39:43 +0800

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,12 @@
+function print(input){
+    if (input === 0){
+        console.log('零')
+    }else if(input>0){
+        console.log('正數')
+    }else if(input<0){
+        console.log('負數')
+    }
+}
+print(0);
+print(7);
+print(-2);
